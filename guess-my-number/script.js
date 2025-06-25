78b5f9f491ab28db9258e5394f8e5980054cdc06
Ashleysteph99 <ashleystephen999@example.com>
2025-06-25 13:31:29 +0530

    added logs

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,3 +1,6 @@
 'use strict';
 console.log(document.querySelector('.label-score').textContent);
 // document.querySelector('.label-score').textContent = "Lol";
+document.querySelector('.guess').value=32
+console.log(document.querySelector('.guess').value); 
+
