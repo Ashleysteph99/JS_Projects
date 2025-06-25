c6819ba0aeb758b5012d0eb03b34f9a3182eaee3
Ashleysteph99 <ashleystephen999@example.com>
2025-06-25 15:17:08 +0530

    Added basic listeners

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,6 +1,10 @@
 'use strict';
-console.log(document.querySelector('.label-score').textContent);
+// console.log(document.querySelector('.label-score').textContent);
 // document.querySelector('.label-score').textContent = "Lol";
-document.querySelector('.guess').value=32
-console.log(document.querySelector('.guess').value); 
+// document.querySelector('.guess').value=32
+// console.log(document.querySelector('.guess').value); 
 
+document.querySelector('.btn.check').addEventListener('click', function () {
+console.log(document.querySelector('.guess').value); 
+    document.querySelector('.message').textContent='Correct Guess!!🎉🎉🎉';
+})
