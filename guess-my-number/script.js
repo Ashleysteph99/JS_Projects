de69d338a50569f7f112f2a5d5b9f807f057f077
Ashleysteph99 <ashleystephen999@example.com>
2025-06-27 11:17:49 +0530

    Added Logic to check entered number and target number and appropirate message

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -4,22 +4,32 @@
 // document.querySelector('.guess').value=32
 // console.log(document.querySelector('.guess').value); 
 
-const target =Math.trunc(Math.random()*20)+1;
+const target = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector('.number').textContent = target;
 
 document.querySelector('.btn.check').addEventListener('click', function () {
-    const guess=document.querySelector('.guess').value.trim(); 
+    const guess = document.querySelector('.guess').value.trim();
     
-    if(!guess){
-        document.querySelector('.message').textContent='Please Enter a Number';
-        console.log(`Target number is ${target}` );
-        console.log(`Guess is ${guess} which is a type of "${typeof(guess)}"`);
+    if (!guess) {
+        document.querySelector('.message').textContent = 'Please Enter a Number';
+        console.log(`Target number is ${target}`);
+        console.log(`Guess is ${guess} which is a type of "${typeof (guess)}"`);
         return;
     }
 
-    const usrGuess= Number(guess); 
-    console.log(`Target number is ${target}` );
-    console.log(`Guess is ${usrGuess} which is a type of "${typeof(usrGuess)}"`);
-    document.querySelector('.message').textContent='Correct Guess!!🎉🎉🎉';
+
+    const usrGuess = Number(guess);
+
+    if (target == usrGuess) {
+        document.querySelector('.message').textContent = 'Correct Guess!!🎉🎉🎉';
+    }
+    else {
+        document.querySelector('.message').textContent = 'Oops, Try Again';
+    }
+    console.log(`Target number is ${target}`);
+    console.log(`Guess is ${usrGuess} which is a type of "${typeof (usrGuess)}"`);
+
 });
 
 
