e73c3b6f1f7e695930f7cb411bf27a9ce99cce79
Ashleysteph99 <ashleystephen999@example.com>
2025-06-28 17:07:08 +0530

    Added reset logic and kept highscore session wise

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -5,13 +5,33 @@
 // console.log(document.querySelector('.guess').value); 
 
 let target = Math.trunc(Math.random() * 20) + 1;
-
 // document.querySelector('.number').textContent = target;
 
-let OgScore = 30;
-let gameOver = false;
+const resetTarget = () => {
+    target = Math.trunc(Math.random() * 20) + 1;
+    console.log(target);
+}
 
-document.querySelector('.score').textContent = OgScore;
+const OgScore = 30;
+let currentScore = OgScore;
+let gameOver = false;
+let highScore = 0;
+
+document.querySelector('.score').textContent = currentScore;
+
+document.querySelector('.btn.again').addEventListener('click', function () {
+    gameOver = false;
+    let OgScore = 30;
+    document.querySelector('.score').textContent = OgScore;
+    document.querySelector('body').style.background = '#222';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.number').style.width = '15rem';
+    // document.querySelector('.highscore').textContent = 0;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    resetTarget();
+    currentScore = OgScore;
+
+});
 
 document.querySelector('.btn.check').addEventListener('click', function () {
 
@@ -31,27 +51,33 @@ document.querySelector('.btn.check').addEventListener('click', function () {
 
     if (target == usrGuess) {
         document.querySelector('.message').textContent = 'Correct Guess!!🎉🎉🎉 You Won Yay';
-        document.querySelector('.highscore').textContent = OgScore;
+
+        if (highScore < currentScore) document.querySelector('.highscore').textContent = currentScore;
         document.querySelector('body').style.background = '#60b347';
         document.querySelector('.number').textContent = target;
+        document.querySelector('.number').style.width = '30rem';
         gameOver = true;
+        highScore = Number(document.querySelector('.highscore').textContent);
+        console.log(`Highscore is${highScore} and currentscore is ${currentScore}`);
+
     }
     else if (usrGuess < target) {
         document.querySelector('.message').textContent = 'Oops, Think Bigger';
-        document.querySelector('.score').textContent = --OgScore;
-        document.querySelector('body').style.background = '#222';
+        document.querySelector('.score').textContent = --currentScore;
+
     }
     else {
         document.querySelector('.message').textContent = 'Oops, Think Lower';
-        document.querySelector('.score').textContent = --OgScore;
-        document.querySelector('body').style.background = '#222';
+        document.querySelector('.score').textContent = --currentScore;
+
     }
     console.log(`Target number is ${target}`);
     console.log(`Guess is ${usrGuess} which is a type of "${typeof (usrGuess)}"`);
 
-    if (OgScore <= 0) {
+    if (currentScore <= 0) {
         document.querySelector('.message').textContent = 'You lost the Game!!!';
         document.querySelector('.btn.check').disabled = true;
+        gameOver = true;
     }
 
 });
